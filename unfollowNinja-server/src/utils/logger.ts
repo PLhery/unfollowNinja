4124328e35fdd1cbe54a273a0d5678c0d43d35e9
plhery <paullouis.hery@gmail.com>
2018-07-27 19:41:23 +0100

    write logs to files

--- unfollowNinja-server/src/utils/logger.ts
+++ unfollowNinja-server/src/utils/logger.ts
@@ -4,17 +4,46 @@ import { createLogger, format, transports } from 'winston';
 const workerInfo = worker ? `work ${worker.id}` : 'master';
 
 const customFormat = format.combine(
-    format.colorize(),
     format.timestamp(),
     format.splat(),
     format.printf((info) => `${info.timestamp} ${workerInfo} ${info.level}: ${info.message}`),
 );
 
+const fileParams = {
+    maxsize: 5000000, // 5MB
+    maxFiles: 200,
+};
+
 const logger = createLogger({
     format: customFormat,
-    level: 'debug',
-    transports: [ new transports.Console() ],
-    exceptionHandlers: [ new transports.Console() ],
+    transports: [
+        new transports.Console({
+            format: format.combine(format.colorize(), customFormat),
+            level: 'debug',
+        }),
+        new transports.File({
+            ...fileParams,
+            filename: 'logs/info.log',
+            level: 'info',
+        }),
+        new transports.File({
+            ...fileParams,
+            filename: 'logs/error.log',
+            level: 'error',
+        }),
+        new transports.File({
+            ...fileParams,
+            filename: 'logs/debug.log',
+            level: 'debug',
+        }),
+    ],
+    exceptionHandlers: [
+        new transports.Console(),
+        new transports.File({
+            ...fileParams,
+            filename: 'logs/exceptions.log',
+        }),
+    ],
 });
 
 if (typeof global.it === 'function') { // mocha
