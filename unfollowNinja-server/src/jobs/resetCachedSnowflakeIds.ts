1dc69aa2ec3b49124bf0be8fec2ab06dbb6fe3fe
plhery <paullouis.hery@gmail.com>
2018-08-18 14:18:57 +0200

    add a one-off job to clean db

---
+++ unfollowNinja-server/src/jobs/resetCachedSnowflakeIds.ts
@@ -0,0 +1,15 @@
+import * as Redis from 'ioredis';
+import Dao from '../dao/dao';
+
+// ran once (while it was in beta) to fix an inconsistency in the DB
+async function run() {
+    const redis = new Redis();
+    const dao = new Dao(redis);
+    const userIds = await dao.getUserIds();
+    for (const userId of userIds) {
+        await redis.del(`followers:follow-time::${userId}`); // typo
+        await redis.del(`followers:snowflake-ids:${userId}`); // inconsistency
+    }
+    redis.disconnect();
+}
+run();
