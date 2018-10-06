edb5b95c092ae72f03cf042e3c766eb7179361ff
plhery <paullouis.hery@gmail.com>
2018-10-06 17:16:33 +0100

    upgrade deps (major jest&gulp)

--- unfollowNinja-server/gulpfile.js
+++ unfollowNinja-server/gulpfile.js
@@ -2,13 +2,15 @@ const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", ["compile"], function () {
-    return gulp.watch("./src/**/*.ts", ["compile"]);
-});
-
 gulp.task("compile", function () {
     return gulp.src("./src/**/*.ts")
         .pipe(tsProject())
         .on('error', () => {})
         .js.pipe(gulp.dest("dist"));
 });
+
+gulp.task("watch", function () {
+    return gulp.watch("./src/**/*.ts", gulp.series("compile"));
+});
+
+gulp.task("default", gulp.parallel("compile", "watch"));
