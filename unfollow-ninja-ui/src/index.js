26bf152f444c20c1bf3039371e6696e4913d7808
Paul-Louis Hery <paullouis.hery@gmail.com>
2020-04-08 23:18:07 +0200

    enable service worker (pwa)

--- unfollow-ninja-ui/src/index.js
+++ unfollow-ninja-ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hydrate, render } from "react-dom";
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
@@ -14,4 +15,4 @@ if (rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.register();
