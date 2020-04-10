f7e8f0ce0288c77f8c106e6874909eec2adc4d60
Paul-Louis Hery <paullouis.hery@gmail.com>
2020-04-10 23:00:18 +0200

    remove legacy ui

--- unfollowNinja-legacyUI/config.default.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.URL =  "https://unfollowninja.fr/"; //L'url publique de votre site (ex http://127.0.0.1:8080/ )
-config.API_URL =  "http://localhost:2000/"; //L'url de l'API
-config.port = 8080; //Le port sur lequel l'app doit écouter
-
-config.sessionSecret = "This is my funky secret oh my god it has ninja turtles"; //Ecrivez ce que vous voulez, permet d'eviter une session hijacking
-
-module.exports = config;
