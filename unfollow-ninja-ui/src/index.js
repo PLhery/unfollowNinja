f4554229db85741fb57cc0b30b0d467fde94520d
Paul-Louis Hery <paullouis.hery@gmail.com>
2020-04-12 18:44:01 +0200

    [UI] more aggressive service workers updates

--- unfollow-ninja-ui/src/index.js
+++ unfollow-ninja-ui/src/index.js
@@ -15,4 +15,12 @@ if (rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: registration => {
+    // reload the page if there is an update
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  }
+});
