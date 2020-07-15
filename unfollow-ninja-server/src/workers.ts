cd462aa134ddf52fa6f181ad25cbf34b6d26df79
Paul-Louis Hery <paullouis.hery@gmail.com>
2020-07-15 16:29:57 +0200

    [server] no need to clean the checkFollowers/... tasks before starting up anymore

--- unfollow-ninja-server/src/workers.ts
+++ unfollow-ninja-server/src/workers.ts
@@ -14,10 +14,6 @@ import logger from './utils/logger';
 import Scheduler from './utils/scheduler';
 import Metrics from './utils/metrics';
 
-// these will be deleted before launching the workers
-const CLEAN_TYPES = ['checkFollowers', 'createTwitterTasks', 'getFollowersInfos', 'cacheFollowers'];
-const CLEAN_STATES = ['delayed', 'inactive'];
-
 // parsing process.env variables
 const CLUSTER_SIZE = Number(process.env.CLUSTER_SIZE) || cpus().length;
 const WORKER_RATE_LIMIT = Number(process.env.WORKER_RATE_LIMIT) || 15;
@@ -61,15 +57,8 @@ if (cluster.isMaster) {
         logger.info('Connected to the kue redis server');
     });
 
-    logger.info('Cleaning the previously created delayed jobs');
-    Promise.all(CLEAN_TYPES.map((type) => Promise.all(CLEAN_STATES.map((state) =>
-            promisify(kue.Job.rangeByType)(type, state, 0, -1, 'asc')
-                .then((jobs: kue.Job[]) => Promise.all(
-                    jobs.map(job => promisify((cb) => job.remove(cb))()),
-                )),
-        ),
-    )))
-        .then(() => dao.load())
+    logger.info('Connecting to the databases..');
+    dao.load()
         .then(() => {
             logger.info('Launching the %s workers...', CLUSTER_SIZE);
             for (let i = 0; i < 2*CLUSTER_SIZE; i++) {
