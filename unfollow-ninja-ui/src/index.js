846228fd865adc6b193e78786cfe529ba664b520
Paul-Louis Hery <paullouis.hery@gmail.com>
2020-10-31 00:58:21 +0100

    fix(service-worker): service-worker was broken since react-scripts 4

--- unfollow-ninja-ui/src/index.js
+++ unfollow-ninja-ui/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { hydrate, render } from "react-dom";
 import App from './App';
 import * as Sentry from '@sentry/browser';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const DSN = process.env.REACT_APP_SENTRY_DSN;
 if (DSN) {
@@ -28,5 +29,19 @@ serviceWorker.register({
       registration.waiting.postMessage({ type: 'SKIP_WAITING' });
     }
     window.location.reload();
+  }
+});
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(({ id, name, value }) => {
+  if (window.gtag) {
+    window.gtag('event', name, {
+      'event_category': 'Web Vitals',
+      'event_label': id,
+      value: Math.round(name === 'CLS' ? value * 1000 : value),
+      'non_interaction': true
+    });
   }
 });
