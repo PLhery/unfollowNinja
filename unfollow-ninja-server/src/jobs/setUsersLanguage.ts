9beb8a17f1e954af333066ad522dba782b737a17
Paul-Louis Hery <paullouis.hery@gmail.com>
2021-11-26 18:00:26 +0100

    fix(server): add job to fix umonkey's user language

---
+++ unfollow-ninja-server/src/jobs/setUsersLanguage.ts
@@ -0,0 +1,22 @@
+import Dao from '../dao/dao';
+import logger from '../utils/logger';
+
+// Following a bug, some unfollowMonkey users have language: fr in their settings. Set it back to fr
+async function runJob() {
+    const dao = await new Dao().load();
+    const userIds = await dao.getUserIds();
+    logger.info(`${userIds.length}`);
+    let i = 0;
+    for (const userId of userIds) {
+        logger.info(`processing ${userId} (${++i}/${userIds.length}...`);
+        const userDao = dao.getUserDao(userId);
+        const lang = await userDao.getLang();
+        if (lang !== 'en') {
+          logger.info('overriding lang for user ' + userId);
+          await userDao.setUserParams({lang: 'en'});
+        }
+    }
+    dao.disconnect();
+}
+
+runJob();
