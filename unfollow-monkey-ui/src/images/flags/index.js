7602b8b34aa8bb4498208387911db6c4befd680f
Paul-Louis Hery <paullouis.hery@gmail.com>
2021-12-02 16:43:37 +0100

    fix(ui): missing german flag

--- unfollow-monkey-ui/src/images/flags/index.js
+++ unfollow-monkey-ui/src/images/flags/index.js
@@ -3,3 +3,4 @@ export {default as en} from './en.svg';
 export {default as es} from './es.svg';
 export {default as pt} from './pt.svg';
 export {default as id} from './id.svg';
+export {default as de} from './de.svg';
