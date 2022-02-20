e4cbc81a6560202dc1a1310c81430a6dbbe3bf51
Paul-Louis Hery <paul-louis.hery@dashlane.com>
2022-02-20 18:24:20 +0100

    chore(ui): upgrade dependencies

--- unfollow-monkey-ui/src/images/flags/index.js
+++ unfollow-monkey-ui/src/images/flags/index.js
@@ -9,6 +9,6 @@ export {default as pl} from './pl.svg';
 export {default as zh_Hans} from './cn.svg';
 export {default as nl} from './nl.svg';
 export {default as tr} from './tr.svg';
-export {default as uk} from './uk.svg';
+export {default as uk} from './ua.svg';
 export {default as pt_BR} from './br.svg';
 export {default as ar} from './eg.svg';
