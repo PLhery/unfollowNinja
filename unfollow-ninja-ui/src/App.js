1649464a426aec60365373f3e59b096ca8c93aeb
Paul-Louis Hery <paullouis.hery@gmail.com>
2022-07-01 19:21:59 +0200

    feat(ui): update github project stars

--- unfollow-ninja-ui/src/App.js
+++ unfollow-ninja-ui/src/App.js
@@ -79,8 +79,8 @@ function App() {
               <Paragraph>Merci à Hivane d'aider le projet à rester performant, libre, et gratuit, soutenant 100 000+ utilisateurs actifs.</Paragraph>
 			  <Paragraph>UnfollowNinja s'appuie sur la librairie node <i>twitter-api-v2</i>, par le même auteur.</Paragraph>
               <Box gap='small' alignSelf='center' direction='row'>
-				<Repo title='unfollowNinja' description='Get notified when your Twitter account loses a follower.' stars={140} forks={12}/>
-				<Repo title='node-twitter-api-v2' description='Strongly typed, full-featured, light, versatile yet powerful Twitter API v1.1 and v2 client for Node.js.' stars={182} forks={12}/>
+				<Repo title='unfollowNinja' description='Get notified when your Twitter account loses a follower.' stars={175} forks={18}/>
+				<Repo title='node-twitter-api-v2' description='Strongly typed, full-featured, light, versatile yet powerful Twitter API v1.1 and v2 client for Node.js.' stars={603} forks={75}/>
               </Box>
             </Box>
           </Box>
