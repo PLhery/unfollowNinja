e5ab5e6bc55f9c16385c7199ab3b746a91bda802
Paul-Louis Hery <paullouis.hery@gmail.com>
2023-01-28 16:39:12 +0100

    feat(server): in case of error with unknown username, log the userid

--- unfollow-ninja-server/src/workers.ts
+++ unfollow-ninja-server/src/workers.ts
@@ -83,8 +83,10 @@ if (cluster.isMaster) {
             bullQueue
                 .process(taskName, WORKER_RATE_LIMIT, (job) =>
                     task.run(job).catch(async (err) => {
-                        const username = job.data.userId ? await dao.getCachedUsername(job.data.userId) : null;
-                        logger.error(`An error happened with ${taskName} / @${username || ''}: ${err.stack}`);
+                        const username = job.data.userId
+                            ? await dao.getCachedUsername(job.data.userId)
+                            : job.data.userId;
+                        logger.error(`An error happened with ${taskName} / @${username}: ${err.stack}`);
                         Sentry.withScope((scope) => {
                             scope.setTag('task-name', taskName);
                             scope.setUser({ username });
