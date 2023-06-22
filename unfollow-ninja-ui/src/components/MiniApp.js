b10bfac49562db8480b3b71b27da71eca177c7d0
Paul-Louis Hery <paullouis.hery@gmail.com>
2023-06-22 23:15:37 +0200

    [ninja ui] fix goodbye text (missing newline)

--- unfollow-ninja-ui/src/components/MiniApp.js
+++ unfollow-ninja-ui/src/components/MiniApp.js
@@ -13,7 +13,7 @@ function MiniApp(props) {
 		  <Paragraph textAlign='center'><Alert/><br/>
               [Avril 2023] Elon Musk a décidé de désactiver les applications Twitter gratuites, y compris <Link href="https://twitter.com/unfollowninja">@UnfollowNinja</Link>.<br/><br/>
               Il n'est donc plus possible de se connecter.<br/>
-              Merci pour votre soutien depuis 2013, c'était fun ♥️<br/>
+              Merci pour votre soutien depuis 2013, c'était fun ♥️<br/><br/>
               [Juin 2023] La version anglaise que je maintiens, <Link href='https://unfollow-monkey.com'>Unfollow Monkey</Link>, est de nouveau disponible!
           </Paragraph>
       </Box>
